1c959a717853fe94e1272c90928c12031e44b058
Tud0r420 <tudoreusebiuconstantin@iesmontsia.org>
2023-03-17 16:11:23 +0100

    dashboard admin

--- resources/views/login/dashboard.blade.php
+++ resources/views/login/dashboard.blade.php
@@ -12,8 +12,12 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
                     {{-- {{ __("You're logged in!") }} --}}
+
                     <div id="app">
                         <welcome/>
+                    </div>
+                    <div id="graphic">
+                        <chart/>
                     </div>
                 </div>
             </div>
