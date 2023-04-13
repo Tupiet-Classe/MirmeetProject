ae4feaab1b0da4493df68abc2894b5b722f9ae07
Tud0r420 <tudoreusebiuconstantin@iesmontsia.org>
2023-04-13 18:38:28 +0200

    fake api swarm

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ApiFakeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/post', [ApiFakeController::class, 'store']);
+Route::get('/get/{reference}', [ApiFakeController::class, 'show']);
