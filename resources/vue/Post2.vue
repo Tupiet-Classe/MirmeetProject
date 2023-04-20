a5a6dbda30cb540ed29d763d3b5202e35a829e05
ZaynOssama <oussamazazou@iesmontsia.org>
2023-04-20 19:02:27 +0200

    Resetear File Input working 1000%

--- resources/vue/Post2.vue
+++ resources/vue/Post2.vue
@@ -29,7 +29,7 @@
         <div class="mb-3 w-96">
           <input name="image"
             class="relative m-0 block w-full min-w-0 flex-auto rounded border border-solid border-neutral-300 dark:border-neutral-600 bg-clip-padding py-[0.32rem] px-3 text-base font-normal text-neutral-700 dark:text-neutral-200 transition duration-300 ease-in-out file:-mx-3 file:-my-[0.32rem] file:overflow-hidden file:rounded-none file:border-0 file:border-solid file:border-inherit file:bg-neutral-100 dark:file:bg-neutral-700 file:px-3 file:py-[0.32rem] file:text-neutral-700 dark:file:text-neutral-100 file:transition file:duration-150 file:ease-in-out file:[margin-inline-end:0.75rem] file:[border-inline-end-width:1px] hover:file:bg-neutral-200 focus:border-primary focus:text-neutral-700 focus:shadow-[0_0_0_1px] focus:shadow-primary focus:outline-none"
-            type="file" @change="onFileChange" accept="image/*,video/*" />
+            type="file" @change="onFileChange" ref="fileInput" accept="image/*,video/*" />
         </div>
       </div>
 
@@ -83,8 +83,9 @@ export default {
   },
   methods: {
     closeModal(){
-      this.file = ''
-      this.text = ''
+      this.$refs.fileInput.value = null;
+      this.file = null
+      this.text = null
       this.showModal = false;
     },
     doAction() {
