03cddcb2ab9ac4db6580295f4692aa5e72be6b42
Tud0r420 <tudoreusebiuconstantin@iesmontsia.org>
2023-04-27 17:02:44 +0200

    estil fondo modal captcha

--- resources/views/auth/login-modal.blade.php
+++ resources/views/auth/login-modal.blade.php
@@ -2,7 +2,7 @@
 <style>
     .bg-gray-900{
     --tw-bg-opacity: 0 !important;
-    background-color: transparent !important;
+    background-color: rgba(46, 46, 46, 0.5);
 }
 </style>
 <div id="loginModal" tabindex="-1" aria-hidden="true" class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
