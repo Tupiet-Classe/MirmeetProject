39f2b2cc0aaa7dd0751103197b2542ebf998397b
JuliaK18 <92729717+JuliaK18@users.noreply.github.com>
2023-05-04 17:52:54 +0200

    Problema arreglat botó Perfil

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,8 +1,27 @@
 {{-- Traduit --}}
-@section('content')
-    <lang-navbar />
-@endsection
 
+<nav class="bg-white border-gray-200 dark:bg-gray-900">
+    <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-2">
+        <div class="hidden w-full md:block md:w-auto" id="navbar-default">
+            {{ app()->getLocale() }}
+            <ul
+                class="font-medium flex flex-col p-2 md:p-0 mt-2 border border-gray-100 rounded-lg bg-gray-100 md:flex-row md:space-x-4 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 justify-end">
+                <li>
+                    <button @click="setLocale('en')" class="language-button block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent text-xs">EN</button>
+                </li>
+                <li>
+                    <button @click="setLocale('es')" id="es" href="{{ url('es') }}"
+    class="language-button block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent text-xs">ES</button>
+
+                </li>
+                <li>
+                    <button id="ca" href="{{ url('ca') }}"
+                        class="language-button block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent text-xs">CA</button>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
 
 <nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
     <!-- Primary Navigation Menu -->
